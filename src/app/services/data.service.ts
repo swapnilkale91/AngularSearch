4b00a042c461d66ebd453cc5f8b0e2af057d971d
kale <212782426@ge.com>
2020-12-26 19:55:48 +0530

    changing the data service layer to address to cors issue

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Inject } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { Options } from 'selenium-webdriver/chrome';
 
 @Injectable({
 	providedIn: 'root'
@@ -10,7 +11,7 @@ export class DataService {
 	public constructor(private http: HttpClient, @Inject('BACKEND_API_URL') private apiUrl: string) { }
 
 	public getData(uri: string): Observable<Object> {
-		let url = this.apiUrl + uri;
-		return this.http.get(url);
+		//let url = this.apiUrl + uri;
+		return this.http.get(uri);
 	}
 }
